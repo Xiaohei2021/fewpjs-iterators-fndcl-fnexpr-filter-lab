56bf66be8f15d04820a89c934126c7c1ddc08f5c
Leon <hualiang.tian@gmail.com>
2021-09-21 22:31:10 -0400

    commit tree 18bdd4d8bf48d7c04bbd2aacc6ab692f4b32deab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(array, string)
+{
+    return array.filter(target =>
+    {
+        return target.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string)
+{
+    return array.filter(target =>
+        {
+            return target.toLowerCase().indexOf(string.toLowerCase()) === 0
+        })
+ }
+
+
+
+function matchName(array, string)
+{
+    const stringName = array.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+    return stringName
+}
